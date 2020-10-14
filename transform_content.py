0465b8828562a8641e3b39d541a1df50414e9d6e
Celsoft <maksim_blg@mail.ru>
2020-10-14 14:22:28 +0300

    ssl always

--- transform_content.py
+++ transform_content.py
@@ -91,4 +91,4 @@ REPLACEMENT_REGEXES = [
 ################################################################################
 
 def TransformContent(base_url, accessed_url, content):
-  return content.replace('https://apparatov.net/', '/')
+  return content.replace('https://igrovyeavtomatyc.com/', '/')
